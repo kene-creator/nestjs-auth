75ad86dcf4d200d39de775198baa157469b06680
Kene <kayyungx@gmail.com>
2023-04-24 16:00:52 +0100

    Add graphql configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLISODateTime, GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
+      resolvers: { DateTime: GraphQLISODateTime },
     }),
     AuthModule,
     PrismaModule,
