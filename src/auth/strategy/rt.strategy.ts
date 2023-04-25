f951b33de1c43bdf5543291da8f8eb13bff6be79
Kene <kayyungx@gmail.com>
2023-04-25 09:17:26 +0100

    Add refresh token strategy

--- src/auth/strategy/rt.strategy.ts
+++ src/auth/strategy/rt.strategy.ts
@@ -1,1 +1,19 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Request } from 'express';
 
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false, //! TODO: set to true in production
+      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(req: Request, payload: any) {
+    return payload;
+  }
+}
