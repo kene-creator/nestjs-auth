c3fb004a000cbdf7d1aa89a29777d81112fec4e2
Kene <kayyungx@gmail.com>
2023-04-29 11:46:25 +0100

    update setup integration

--- test/integration/create-user.integration.spec.ts
+++ test/integration/create-user.integration.spec.ts
@@ -1,1 +1,13 @@
-describe('create-user', () => {});
+import { IntegrationManager } from '../../src/test/IntegrationTestManager';
+
+describe('create-user', () => {
+  const integrationTestManager = new IntegrationManager();
+
+  beforeAll(async () => {
+    await integrationTestManager.beforeAll();
+  });
+
+  afterAll(async () => {
+    await integrationTestManager.afterAll();
+  });
+});
