5d4af008df8fa3d65a18836385916011512c8237
Kene <kayyungx@gmail.com>
2023-04-29 12:38:13 +0100

    Add test for when a new user mutation is created

--- test/integration/create-user.integration.spec.ts
+++ test/integration/create-user.integration.spec.ts
@@ -1,4 +1,8 @@
+import { User } from '@prisma/client';
 import { IntegrationManager } from '../../src/test/IntegrationTestManager';
+import request from 'supertest-graphql';
+import gql from 'graphql-tag';
+import { testUser } from '../stubs/user.stubs';
 
 describe('create-user', () => {
   const integrationTestManager = new IntegrationManager();
@@ -9,5 +13,39 @@ describe('create-user', () => {
 
   afterAll(async () => {
     await integrationTestManager.afterAll();
+  });
+
+  describe('given that the user does not already exist', () => {
+    describe('when a create user mutation is executed', () => {
+      let createdUser: User;
+
+      beforeAll(async () => {
+        const response = await request<{ createUser: User }>(
+          integrationTestManager,
+        )
+          .mutate(
+            gql`
+              mutation Mutation($createUserInput: CreateUserInput!) {
+                createUser(createUserInput: $createUserInput) {
+                  email
+                }
+              }
+            `,
+          )
+          .variables({
+            createUserInput: {
+              ...testUser,
+            },
+          })
+          .expectNoErrors();
+        createdUser = response.data.createUser;
+      });
+
+      test('then the the response should be the newly created user', () => {
+        expect(createdUser).toMatchObject({
+          email: testUser.email,
+        });
+      });
+    });
   });
 });
